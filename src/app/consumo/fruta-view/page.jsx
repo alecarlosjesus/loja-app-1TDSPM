eca972aaf88cbded6f0211a8fc93f60b37992b09
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-30 10:57:58 -0300

    Get All frutas.

--- src/app/consumo/fruta-view/page.jsx
+++ src/app/consumo/fruta-view/page.jsx
@@ -17,6 +17,21 @@ export default async function FrutaView() {
     }
 
   return (
-    <div>page</div>
+    <div>
+
+        <h1>Frutas</h1>
+        <div>
+          <ul>
+            {frutas.map((fruta) => (
+              <li key={fruta.id}>
+                <p>{fruta.nome}</p>
+                <p>{fruta.tipo}</p>
+                <p>{fruta.desc}</p>
+                <hr />
+              </li>
+            ))}
+          </ul>
+        </div>
+    </div>
   )
 }
